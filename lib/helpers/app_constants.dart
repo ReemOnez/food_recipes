35acfaf077b84a1d96769e7472e93b8200d2317b
reemy <reem66onez@gmail.com>
2025-06-08 14:13:54 +0300

    add helpers folder with its files routes, app_constants

---
+++ lib/helpers/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static const String languageKEY = 'lang';
+  static const String tokenKEY = 'token';
+  static const String arabicLanguage = "ar";
+  static const String englishLanguage = "en";
+
+  static const userId = 'CACHED_USER_ID';
+  static const userEmail = 'CACHED_USER_EMAIL';
+  static const userName = 'CACHED_USER_NAME';
+  static const token = 'CACHED_TOKEN';
+}
